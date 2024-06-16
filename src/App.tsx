5e4c115486ec8c8ede96325a16aef3de4cb000b7
Pan_Janke <jan.krol.legowski@gmail.com>
2024-06-16 21:40:40 +0200

    get data from dummy backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
-function App() {
-  return <h1>Data Fetching!</h1>;
+import {ReactNode, useEffect, useState} from "react";
+import {get} from "./util/http.ts";
+import BlogPosts, {BlogPost} from "./components/BlogPosts.tsx";
+import fetchingImg from './assets/data-fetching.png'
+
+
+type RawDataBlogPost = {
+    id: number;
+    userId: number;
+    title: string;
+    body: string;
 }
+
+function App() {
+    const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+    useEffect(() => {
+        async function fetchPosts() {
+            const data =
+                await get("https://jsonplaceholder.typicode.com/posts") as RawDataBlogPost[];
+
+            const blogPosts: BlogPost[] = data.map(rawPost=>{
+                return{
+                    id: rawPost.id,
+                    title: rawPost.title,
+                    text: rawPost.body
+                }
+
+            });
+            setFetchedPosts(blogPosts);
+        }
+
+        fetchPosts();
+    }, []);
+
+    let content: ReactNode;
+    if(fetchedPosts)
+        content=<BlogPosts posts={fetchedPosts}/>
+
+    return <main>
+        <img src = {fetchingImg} alt ="buba"/>
+        {content}
+    </main>
+}
+
 
 export default App;
